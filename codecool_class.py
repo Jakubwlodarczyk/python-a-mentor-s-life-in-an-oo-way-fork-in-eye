e970f10b10e84ab762679670ebfa34ccdaa93a29
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-01-10 14:43:56 +0100

    story

--- codecool_class.py
+++ codecool_class.py
@@ -3,4 +3,31 @@ from student import Student
 
 
 class CodecoolClass:
-    pass
+    
+    
+	def create_local_school():
+		print('\ncreate_local_school')
+			
+	def presentation():
+	    print('\npresentation')
+
+
+	def call_up():
+	    print('\ncall_up')
+
+	
+	def cofee():
+	    print('\ncofee')
+
+
+	def private_mentoring():
+	    print('\nprivate_mentoring')
+
+	def checkpoing():
+	    print('\nheckpoing')
+	
+
+	
+	
+
+
