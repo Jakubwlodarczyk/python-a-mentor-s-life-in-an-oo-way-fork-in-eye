939e44bdf9a4fc5247b58533fea7f5bdd3337a48
fulloffish <tunczykzcytryna@gmail.com>
2017-01-10 15:20:45 +0100

    Method create_by_csv in progress

--- student.py
+++ student.py
@@ -1,14 +1,27 @@
 from person import Person
-
+import csv
 
 class Student(Person):
 
-    def __init__(self, knowledge, motivation, sweets):
+    def __init__(self, first_name, last_name, year_of_birth, gender, coffee, energy, knowledge, motivation, sweets):
         Person.__init__(self, first_name, last_name, year_of_birth, gender, coffee, energy)
         self.knowledge = knowledge
         self.motivation = motivation
         self.sweets = sweets
 
-    def create_by_csv():
-        #return list of students
-        pass
+    @classmethod
+    def create_by_csv(cls, file_path):
+
+        try:
+            with open('data/students.csv') as students_data:
+                student_list = csv.reader(students_data, quotechar=",")
+                list_of_students =[]
+                for student in student_list:
+                    students = Student(student[0], student[1], student[2], student[3], student[4], student[5], student[6], student[7], student[8])
+                    list_of_students.append(students)
+        except FileNotFoundError:
+            print("\nOops! The file does not exist!")
+        #return students
+        print(list_of_students)
+
+Student.create_by_csv('data/students.csv')
