846cab65de7deef40eb1a74923f6e2c122a6a3c2
Jakub <kubawl.91@gmail.com>
2017-01-10 20:15:55 +0100

    changes in person.py

--- person.py
+++ person.py
@@ -2,22 +2,19 @@ class Person:
 
     def __init__(self, first_name, last_name, year_of_birth, gender, coffee, energy):
 
-        # boolean_list = ['True', 'False']
-        # gender_list = ['male', 'female', 'notsure']
-
-        # # below we check if the attributes are empty, or not valid
-        # if first_name is None:
-        #     raise TypeError("Please write only a letters")
-        # if last_name is None:
-        #     raise TypeError("Please write only a letters")
-        # if year_of_birth is None:
-        #     raise TypeError("Please write only a year of birth")
-        # if gender not in gender_list:
-        #     raise TypeError("Please choose from male/female/notsure")
-        # if coffee not in boolean_list:
-        #     raise TypeError("Please write True or False")
-        # if energy is None:
-        #     raise TypeError("Please write from 0-100")
+        # below we check if the attributes are empty
+        if first_name is None:
+            raise TypeError("First name cannot be empty")
+        if last_name is None:
+            raise TypeError("Last name cannot be empty")
+        if year_of_birth is None:
+            raise TypeError("Year of birth cannot be empty")
+        if gender is None:
+            raise TypeError("Please write a gender (male/female/notsure)")
+        if coffee is None:
+            raise TypeError("Please write True or False")
+        if energy is None:
+            raise TypeError("Please write from 0-100")
 
         self.first_name = first_name
         self.last_name = last_name
