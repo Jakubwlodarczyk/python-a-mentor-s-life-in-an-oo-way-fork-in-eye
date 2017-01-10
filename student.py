f0a2d6b66adc9b8c2387749a59ec0e42e3810c6a
fulloffish <tunczykzcytryna@gmail.com>
2017-01-10 13:40:21 +0100

    method __init__ added

--- student.py
+++ student.py
@@ -2,4 +2,12 @@ from person import Person
 
 
 class Student(Person):
-    pass
+
+    def __init__(self, knowledge, motivation, sweets):
+        Person.__init__(self, first_name, last_name, year_of_birth, gender, coffee, energy)
+        self.knowledge = knowledge
+        self.motivation = motivation
+        self.sweets = sweets
+
+    def create_by_csv():
+        #return list of students
