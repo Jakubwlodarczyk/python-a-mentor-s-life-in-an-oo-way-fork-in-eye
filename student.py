e56f282bab7ef940c0f38ad3995302ef54643d3e
fulloffish <tunczykzcytryna@gmail.com>
2017-01-11 13:25:02 +0100

    Changed file path

--- student.py
+++ student.py
@@ -24,7 +24,7 @@ class Student(Person):
     def create_by_csv(cls, file_path):
 
         try:
-            with open('data/students.csv') as students_data:
+            with open(file_path) as students_data:
                 student_list = csv.reader(students_data, quotechar=",")
                 list_of_students =[]
                 for student in student_list:
