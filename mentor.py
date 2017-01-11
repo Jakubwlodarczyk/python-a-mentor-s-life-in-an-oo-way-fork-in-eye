481a7ede7a97801281001682f73a790aa69bca17
fulloffish <tunczykzcytryna@gmail.com>
2017-01-11 16:26:41 +0100

    Changed data type of sweets from str to int

--- mentor.py
+++ mentor.py
@@ -3,18 +3,13 @@ import csv
 
 
 class Mentor(Person):
-    def __init__(
-                 self, first_name, last_name, year_of_birth, gender,
-                 coffee, energy, nickname, involvment, irritation, sweets
-                 ):
-        Person.__init__(
-                        self, first_name, last_name,
-                        year_of_birth, gender, coffee, energy
-                        )
+
+    def __init__(self, first_name, last_name, year_of_birth, gender, coffee, energy, nickname, engagement, irritation, sweets):
+        Person.__init__(self, first_name, last_name, year_of_birth, gender, coffee, energy)
         self.nickname = nickname
-        self.involvment = involvment
+        self.engagement = engagement
         self.irritation = irritation
-        self.sweets = sweets
+        self.sweets = int(sweets)
 
     def __str__(self):
         return "{} {} {} ".format(self.first_name,  self.last_name, self.nickname)
@@ -23,12 +18,11 @@ class Mentor(Person):
     def create_by_csv(cls, filepath):
         try:
             with open('data/mentors.csv') as mentor_file:
-                all_mentors = csv.reader(mentor_file, delimiter=',', quotechar='|')
+                all_mentors = csv.reader(mentor_file, delimiter=',', quotechar=',')
                 mentors_list = []
                 for mentor in all_mentors:
-                    mentors = Mentor(mentor[0], mentor[1], mentor[2],
-                                     mentor[3], mentor[4], mentor[5],
-                                     mentor[6], mentor[7], mentor[8], mentor[9])
+                    mentors = Mentor(mentor[0], mentor[1], mentor[2], mentor[3], mentor[4],
+                                     mentor[5], mentor[6], mentor[7], mentor[8], mentor[9])
                     mentors_list.append(mentors)
         except FileNotFoundError:
             print("File does not exist")
@@ -37,4 +31,6 @@ class Mentor(Person):
 Mentor.create_by_csv('data/mentors.csv')
 
 '''mentors_array = Mentor.create_by_csv('data/mentors.csv')
-print(mentors_array[0]) # check elements in my list for now '''
+print(mentors_array[0].sweets)  # check elements in my list for now
+for mentor in mentors_array:  # yay its so brilliant!
+    print(mentor.first_name, mentor.gender)'''
